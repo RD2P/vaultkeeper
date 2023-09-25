8913a960ee9c65d7941f427aa0d4abbd96d87139
Raphael <graphael.dr@gmail.com>
2023-09-24 22:01:02 -0600

    Fixed decryption display bug

--- src/components/AccountRow.jsx
+++ src/components/AccountRow.jsx
@@ -4,19 +4,19 @@ import Decrypt from './Decrypt'
 export default function AccountRow(props){
  
   function handleDecrypt(event) {
-    const pwText = document.querySelector('.password--text')
     const password = props.password
     const decrKey = window.prompt("Please enter key: ")
     const decrypted = (Decrypt(password, decrKey))
-    pwText.textContent = decrypted
+    const accountRowElement = event.target.parentElement.parentElement
+    const passwordText = accountRowElement.querySelector('.password--text')
+    passwordText.textContent = decrypted
   }
-
-  
 
   return (
       <div className="account-row">
         <div>{props.site}</div>
         <div>{props.username}</div>
+        {/* props.password below is the encrypted password stored in the accounts array in AccountRow.jsx*/}
         <div className="password--text">{props.password}</div>
         <div className="account-row--button-container">
           <button className="account-row--button" onClick={handleDecrypt}>Decrypt</button>
